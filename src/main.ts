056fcaedf28ff87689679ff7b25b2ae013d878aa
Luis231968 <202000770@uthh.edu.mx>
2024-07-22 03:49:17 -0600

    ola

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,11 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const CorsOptions: CorsOptions = {
-    origin: ['https://novedades-ar.netlify.app', 'http://localhost:4200'], 
+    origin: ['https://novedades-ar.netlify.app', 'http://localhost:4200','https://novedades-ar-proyecto.vercel.app'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: false,
   }
-
   app.enableCors(CorsOptions);
-
   await app.listen( process.env.PORT || 3000 );
 }
 bootstrap();
