8aa865866c0267ddc7142e7e528d1e593b18e937
ElNao <naopubg28@gmail.com>
2024-11-12 02:19:27 -0600

    conexion con railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,28 +18,28 @@ import { StripeModule } from './stripe/stripe.module';
 import { RatingModule } from './rating/rating.module';
 
 @Module({
-   imports: [
-     TypeOrmModule.forRoot({
-       type: 'mysql',
-       host: 'localhost',//monorail.proxy.rlwy.net
-       port: 3306,//32140 3306
-       username: 'root',
-       password: '',//SOZqfoANqhNxIbtccwkusRwjpdvxfsKf
-       database: 'railway',//railway 
-       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-       synchronize: true,
-     }),
-  //imports: [
-  //  TypeOrmModule.forRoot({
-  //    type: 'mysql',
-  //    host: 'monorail.proxy.rlwy.net',//monorail.proxy.rlwy.net
-  //    port: 32140,//32140 3306
-  //    username: 'root',
-  //    password: 'SOZqfoANqhNxIbtccwkusRwjpdvxfsKf',//SOZqfoANqhNxIbtccwkusRwjpdvxfsKf
-  //    database: 'railway',//railway 
-  //    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  //    synchronize: true,
-  //  }),
+  // imports: [
+  //   TypeOrmModule.forRoot({
+  //     type: 'mysql',
+  //     host: 'localhost',//monorail.proxy.rlwy.net
+  //     port: 3306,//32140 3306
+  //     username: 'root',
+  //     password: '',//SOZqfoANqhNxIbtccwkusRwjpdvxfsKf
+  //     database: 'railway',//railway 
+  //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  //     synchronize: true,
+  //   }),
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'monorail.proxy.rlwy.net',//monorail.proxy.rlwy.net
+      port: 32140,//32140 3306
+      username: 'root',
+      password: 'SOZqfoANqhNxIbtccwkusRwjpdvxfsKf',//SOZqfoANqhNxIbtccwkusRwjpdvxfsKf
+      database: 'railway',//railway 
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     UsersModule,
     EmailModule,
     LoginModule,
