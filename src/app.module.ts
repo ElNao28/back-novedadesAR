08709054642a894c95f44689c1f56fcc4a814dd5
ElNao <naopubg28@gmail.com>
2024-11-14 12:45:35 -0600

    se completo la funcion para registrar pagos de stripe en mobil

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,25 +21,25 @@ import { RatingModule } from './rating/rating.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',//monorail.proxy.rlwy.net
-      port: 3306,//32140 3306
+      host: 'localhost', //monorail.proxy.rlwy.net
+      port: 3306, //32140 3306
       username: 'root',
-      password: '',//SOZqfoANqhNxIbtccwkusRwjpdvxfsKf
-      database: 'railway',//railway 
+      password: '', //SOZqfoANqhNxIbtccwkusRwjpdvxfsKf
+      database: 'railway', //railway
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-  // imports: [
-  //   TypeOrmModule.forRoot({
-  //     type: 'mysql',
-  //     host: 'monorail.proxy.rlwy.net',//monorail.proxy.rlwy.net
-  //     port: 32140,//32140 3306
-  //     username: 'root',
-  //     password: 'SOZqfoANqhNxIbtccwkusRwjpdvxfsKf',//SOZqfoANqhNxIbtccwkusRwjpdvxfsKf
-  //     database: 'railway',//railway 
-  //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  //     synchronize: true,
-  //   }),
+    // imports: [
+    //   TypeOrmModule.forRoot({
+    //     type: 'mysql',
+    //     host: 'monorail.proxy.rlwy.net',//monorail.proxy.rlwy.net
+    //     port: 32140,//32140 3306
+    //     username: 'root',
+    //     password: 'SOZqfoANqhNxIbtccwkusRwjpdvxfsKf',//SOZqfoANqhNxIbtccwkusRwjpdvxfsKf
+    //     database: 'railway',//railway
+    //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //     synchronize: true,
+    //   }),
     UsersModule,
     EmailModule,
     LoginModule,
@@ -53,7 +53,7 @@ import { RatingModule } from './rating/rating.module';
     TestMsjModule,
     PushNotificationsModule,
     StripeModule,
-    RatingModule
+    RatingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,26 +1,54 @@
 import { Injectable } from '@nestjs/common';
 import { CreateStripeDto } from './dto/create-stripe.dto';
 import { UpdateStripeDto } from './dto/update-stripe.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { VentasService } from 'src/ventas/ventas.service';
+import { ResDto } from 'src/products/dto/res.dto';
+
+const stripe = require('stripe')(
+  'sk_test_51Os6QyP0xF5rSbalHiltPXqBNbewYYo0T3P02CikwxwUFGLXZqnfNoHZyC8P03TWCTUxypvbrTQqigaWoWx5ctlf00XocCc2bt',
+);
 
 @Injectable()
 export class StripeService {
-  create(createStripeDto: CreateStripeDto) {
-    return 'This action adds a new stripe';
-  }
+  constructor(private ventasService: VentasService) {}
+  async pagoStripe(data: ResDto[]) {
 
-  findAll() {
-    return `This action returns all stripe`;
-  }
+    let itemsVenta = [];
+    itemsVenta = data.map((data) => {
+      return {
+        idProducto: data.id,
+        cantidad: data.cantidad,
+      };
+    });
+    let total = 0;
+    data.forEach((data) => {
+      total = data.precio += data.precio;
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} stripe`;
-  }
-
-  update(id: number, updateStripeDto: UpdateStripeDto) {
-    return `This action updates a #${id} stripe`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} stripe`;
+    const customer = await stripe.customers.create();
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer.id },
+      { apiVersion: '2024-10-28.acacia' },
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: 'mxn',
+      customer: customer.id,
+    });
+    this.ventasService.addVentaStripe(
+      +data[0].idUser,
+      itemsVenta,
+      total,
+      data[0].idCard,
+      paymentIntent.id,
+    );
+    return {
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+      publishableKey:
+        'pk_test_51Os6QyP0xF5rSbalhCDVxAhQAHMJJLSQsgR9JRdjUrd1MQHuWDxzNNFP84btqgdlTAniH5bX6NEX31jctM7CGuYC00OYXvGDI7',
+    };
   }
 }
